b92afa877506a5b9c3a52efac0786f499dfb29d6
Manav <manavs909@gmail.com>
2026-05-12 00:51:02 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ import Audit     from "./pages/Audit";
 import Admin     from "./pages/Admin";
 import Playground from './pages/Playground';
 import AlgorithmIntel from './pages/AlgorithmIntel';
+import SystemControl from './pages/SystemControl';
 import NotFound  from "./pages/NotFound";
 
 const queryClient = new QueryClient({
